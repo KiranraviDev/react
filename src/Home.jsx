7787924e0704e7be7e8eba3d026618aafde60151
kiranravi_dev <kiranravi.dev@gmail.com>
2024-01-05 02:04:07 +0530

    React logo and API page link addres design Added on Homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,16 +6,20 @@ const Home = () => {
   <div className="w-full">
         <div className="flex justify-center bg-black">
         <img src={reactimg} alt="asd" width={"70px"} />
-        <h1 className="text-4xl p-10 text-white">REACT JS</h1>
+        <h1 className="text-5xl p-10 text-white">Kiran Ravi</h1>
+        <img src={reactimg} alt="asd" width={"70px"} />
         </div>
         <hr />
-        <div className="grid justify-center p-5">
-            <h1 className="text-4xl">Sample Projects</h1>
-        <ol className="text-center m-10"><li>
-            <div className="flex justify-center items-end gap-2">
+        <div className="grid justify-center p-5 bg-gray-900">
+            <h1 className="text-4xl text-gray-400">Mini Projects</h1>
+        <ol className="text-center m-10">
+            <li>
+            <div className="flex justify-center align-bottom gap-2">
             <img src={reactimg} alt="asd" width={"20px"} />
-            <Link to="/api" className=" hover:text-blue-500 align-bottom font-mono">API TABLE</Link>    
+            <Link to="/api" className=" text-gray-600 hover:text-blue-500 align-bottom font-mono">API TABLE</Link>    
             </div>
+            {/*  */}
+        
         </li>
         </ol>
         </div>
